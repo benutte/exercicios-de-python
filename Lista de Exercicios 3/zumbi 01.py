963b403e00cb9d0ab816bcee991fb41ebf578f45
benutte <benutte20@gmail.com>
2018-02-19 15:36:54 -0300

    mais exercicios

---
+++ Lista de Exercicios 3/zumbi 01.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+
+num = int(input(" Digite um numero entre 0 e 10: "))
+
+while num not in (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10):
+    print (" Número inválido... tente novamente!!")
+    num = int(input(" Digite um numero entre 0 e 10: "))
+print num
+        
+    
